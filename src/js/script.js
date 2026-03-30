6e99b625773531868a75f0d66ff5d8f6d3f884f4
Danillo Roque <danilloroquelinkedin@gmail.com>
2026-03-30 14:45:10 -0300

    Chore: Add nome dos vinhos ao console

--- src/js/script.js
+++ src/js/script.js
@@ -2,3 +2,9 @@
 let nome = prompt("Insira seu nome: ");
 let estado = prompt("Insira o seu estado: ")
 alert(`Cadastro realizado!\nSeja bem vindo ${nome}, Veja os detalhes no console.`);
+
+console.log(`Cabernet 2020: `)
+
+console.log(`Sauvignon Blanc 2023: `)
+
+console.log(`Viejo Feo Reserva Pinot Noir Rosé 2024: `)
